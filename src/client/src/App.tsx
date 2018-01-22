eab86280b9fc5aed8cbc90aa121ba2d927e91872
Lulu300 <ludwigsimon77@gmail.com>
2018-01-22 22:59:22 +0100

    Add: tslint rules

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-const logo = require('./logo.svg');
+const logo = require("./logo.svg");
 
 class App extends React.Component {
   render() {
