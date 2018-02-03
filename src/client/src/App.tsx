a92baf456e22eac15587f7e4cf6bea1ca4cf8c03
Lulu300 <ludwigsimon77@gmail.com>
2018-02-03 21:22:15 +0100

    feat: add react-bash

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,22 +1,20 @@
 import * as React from "react";
-import "./App.css";
+import Terminal from "react-bash";
 
-const logo = require("./logo.svg");
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default class App extends React.Component<any, any> {
+    showMsg = () => "Hello World";
+    render() {
+        return (
+            <div 
+                style={{
+                    height: "250px"
+                }}
+            >
+                <Terminal
+                    prefix="code@chill" 
+                    theme={Terminal.Themes.DARK}    
+                />
+            </div>
+        );
+    }
 }
-
-export default App;
