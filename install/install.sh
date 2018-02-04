2cb30275c70fb1b4028ad4393bc9265f669e4e4c
Lulu300 <ludwigsimon77@gmail.com>
2018-02-04 17:57:14 +0100

    build: auto create a startup docker container

--- install/install.sh
+++ install/install.sh
@@ -130,7 +130,7 @@ StartLimitInterval=60s\n
 WantedBy=multi-user.target" > /lib/systemd/system/docker.service
 systemctl daemon-reload                                                                            
 systemctl restart docker
-# export DOCKER_HOST=tcp://localhost:2375
+export DOCKER_HOST=tcp://localhost:2375
 if ! grep -qF "DOCKER_HOST=tcp://localhost:2375" /etc/environment
 then
         echo "DOCKER_HOST=tcp://localhost:2375" >> /etc/environment
@@ -138,7 +138,7 @@ fi
 source /etc/environment
 systemctl restart docker
 docker pull ubuntu
-docker run --name code-chil -i -t ubuntu /bin/bash
+docker run --name code-chill -dti ubuntu /bin/bash
 
 # Install js packages
 cd $client
