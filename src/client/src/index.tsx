5c8d16a2d3c8410f892e5620c6c99143d2470393
AlexandrePetit-Pas <petitpasalexandre@gmail.com>
2018-02-11 16:39:57 +0100

    feat: add react router

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
+import Term, { Terminal } from "./components/Term";
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <div>
+      <Route exact="true" path="/" component={App} />
+      <Route 
+        exact="true" 
+        path="/term" 
+        render={(props) => <Term prefix="code@chill" theme={Terminal.Themes.DARK} />} 
+      />
+    </div>
+  </Router>,
   document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
