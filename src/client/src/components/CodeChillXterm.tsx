8073ed3519cb69c871ca281811dfc5b1293fa232
AlexandrePetit-Pas <petitpasalexandre@gmail.com>
2018-03-18 23:47:31 +0100

    feat: add handle of xterm attach

--- src/client/src/components/CodeChillXterm.tsx
+++ src/client/src/components/CodeChillXterm.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { Terminal } from "xterm";
+import * as Terminal from "xterm/build/xterm";
+import * as attach from "xterm/build/addons/attach/attach";
 import * as ClassName from "classnames";
-import * as TextEncoding from "text-encoding";
 import AuthService from "../AuthService";
 import withAuth from "./withAuth";
 
@@ -46,104 +46,20 @@ class CodeChillXterm extends React.Component<IxTermProps, IxTermState> {
         // this.props.options
         this.msg = "";
         this.Auth = new AuthService();
-
-        this.xterm = new Terminal({
-            cursorBlink: false,  // Do not blink the terminal's cursor
-            cols: 120,  // Set the terminal's width to 120 columns
-            // rows: 50,  // Set the terminal's height to 10 rows
-          
-        });
-            // individual paddings
+        this.xterm = new Terminal();
     }
 
     componentDidMount() {
-        const xt = this;
-        var lastCommand = "";
-
-        this.Auth.startDocker("code-chill").then((res) => {
+        var xt = this;
+        Terminal.applyAddon(attach);
+        this.webSocket = new WebSocket(`ws://localhost:2375/containers/${this.props.user.dockers[0].name}/
+attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1`);
+        this.Auth.startDocker(this.props.user.dockers[0].name).then((res) => {
             console.log(res);
         });
-
-        // if (xt.props.url) {
-        xt.webSocket = new WebSocket(`ws://localhost:2375/containers/code-chill/
-attach/ws?logs=0&stream=1&stdin=0&stdout=0&stderr=0`);
-            // this.webSocket.addEventListener("message", this.recieveData);
-        // }
-
-        xt.webSocket.onopen = function(event: Event) {
-            console.log("connexion");
-        };
-        xt.webSocket.onerror = function() {
-            console.log("onerreur");
-        };
-        xt.webSocket.onmessage = function(event: any) {
-            console.log("onmessage");
-            let decoder = new TextEncoding.TextDecoder("utf-8");
-            var fileReader = new FileReader();
-            fileReader.onload = function() {
-                let decoded = decoder.decode(this.result);
-                console.log(decoded.localeCompare(lastCommand));
-                if (decoded.localeCompare(lastCommand) === -1) {
-                    xt.xterm.write(decoded);
-                }
-            };
-            fileReader.readAsArrayBuffer(event.data);
-        }; 
-
         this.xterm.open(this.refs.container);
-        this.xterm.on("focus", this.focusChanged.bind(this, true));
-        this.xterm.on("blur", this.focusChanged.bind(this, false));
-        this.xterm.on("resize", function(size: any) {
-            xt.xterm.resize(size.cols, size.rows);
-        });
-
-        if (this.props.onContextMenu) {
-            this.xterm.element.addEventListener("contextmenu", this.onContextMenu.bind(this));
-        }
-
-        if (this.props.onInput) {
-            this.xterm.on("data", this.onInput);
-        }
-
-        if (this.props.value) {
-            this.xterm.write(this.props.value);
-        }
-
-        this.getTerminal().on("key", function(key: string, e: KeyboardEvent) {
-            // e: KeyboardEvent; e.key: string; e.which: number
-            if (e.key === "Backspace") {
-                xt.msg = xt.msg.substring(0, xt.msg.length - 1);
-                xt.xterm.write("\b \b");
-            } else if (e.key === "Enter") {
-                xt.msg = xt.msg + key;
-                xt.webSocket.send(xt.msg);   
-                lastCommand = xt.msg;  
-                xt.msg = "";
-                xt.xterm.write("\r\n");
-            } else if (e.key === "Escape" || e.key === "ArrowUp" || e.key === "ArrowDown") {
-                xt.webSocket.send(key);
-            } else {
-                xt.msg = xt.msg + key;
-                xt.xterm.write(key);
-            }
-            // else if (e.ctrlKey === true) {
-                // xt.webSocket.send(key ou xt.msg + key)
-                // a voir comment faire !
-                /**
-                 * TODO CTRL+KEY HANDLER (block ctrl+w) ctrl+a ctrl+c etc.
-                 */
-            // } else if (e.altKey === true) {
-                /**
-                 * TODO ALT+KEY HANDLER
-                 */
-            // } else if (e.shiftKey === true) {
-                /**
-                 *  TODO SHIFT+KEY HANDLER
-                 */
-            // } else if (e.key === "Tab") {
-                /**
-                 *  TODO TAB HANDLER
-                 */
+        this.webSocket.addEventListener("open", function () {
+            (xt.xterm as any).attach(xt.webSocket, xt.webSocket, false, false);
         });
     }
 
@@ -152,52 +68,13 @@ attach/ws?logs=0&stream=1&stdin=0&stdout=0&stderr=0`);
             this.xterm.destroy();
             delete this.xterm;
         }
-        this.Auth.stopDocker("code-chill").then((res) => {
+        this.Auth.stopDocker(this.props.user.dockers[0].name).then((res) => {
             console.log(res);
         });
     }
 
     getTerminal() {
         return this.xterm;
-    }
-
-    focus() {
-        if (this.xterm) {
-            this.xterm.focus();
-        }
-    }
-
-    focusChanged(focused: boolean) {
-        this.setState({
-            isFocused: focused
-        });
-        if (this.props.onFocusChange) {
-            this.props.onFocusChange(focused);
-        }
-    }
-
-    onInput = (data: any) => {
-        if (this.props.onInput) {
-            this.props.onInput(data);
-        }
-    }
-
-    resize (cols: number, rows: number) {
-        this.xterm.resize(Math.round(cols), Math.round(rows));
-    }
-
-    setOption(key: string, value: boolean) {
-        this.xterm.setOption(key, value);
-    }
-
-    refresh() {
-        this.xterm.refresh(0, this.xterm.rows - 1);
-    }
-
-    onContextMenu(e: any) {
-        if (this.props.onContextMenu) {
-            this.props.onContextMenu(e);
-        }
     }
 
     render() {
