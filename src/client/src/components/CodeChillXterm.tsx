6bba9c615f4bc5d69e7181cdc2aa3c2f5825defd
Alexandre Petit-Pas <petitpasalexandre@gmail.com>
2018-03-19 00:00:27 +0100

    refactor: delete unused code

--- src/client/src/components/CodeChillXterm.tsx
+++ src/client/src/components/CodeChillXterm.tsx
@@ -61,55 +61,6 @@ attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1`);
         this.webSocket.addEventListener("open", function () {
             (xt.xterm as any).attach(xt.webSocket, xt.webSocket, false, false);
         });
-
-        if (this.props.onContextMenu) {
-            this.xterm.element.addEventListener("contextmenu", this.onContextMenu.bind(this));
-        }
-
-        if (this.props.onInput) {
-            this.xterm.on("data", this.onInput);
-        }
-
-        if (this.props.value) {
-            this.xterm.write(this.props.value);
-        }
-
-        this.getTerminal().on("key", function(key: string, e: KeyboardEvent) {
-            // e: KeyboardEvent; e.key: string; e.which: number
-            if (e.key === "Backspace") {
-                xt.msg = xt.msg.substring(0, xt.msg.length - 1);
-                xt.xterm.write("\b \b");
-            } else if (e.key === "Enter") {
-                xt.msg = xt.msg + key;
-                xt.webSocket.send(xt.msg);   
-                lastCommand = xt.msg;  
-                xt.msg = "";
-                xt.xterm.write("\r\n");
-            } else if (e.key === "Escape" || e.key === "ArrowUp" || e.key === "ArrowDown") {
-                xt.webSocket.send(key);
-            } else {
-                xt.msg = xt.msg + key;
-                xt.xterm.write(key);
-            }
-            // else if (e.ctrlKey === true) {
-                // xt.webSocket.send(key ou xt.msg + key)
-                // a voir comment faire !
-                /**
-                 * TODO CTRL+KEY HANDLER (block ctrl+w) ctrl+a ctrl+c etc.
-                 */
-            // } else if (e.altKey === true) {
-                /**
-                 * TODO ALT+KEY HANDLER
-                 */
-            // } else if (e.shiftKey === true) {
-                /**
-                 *  TODO SHIFT+KEY HANDLER
-                 */
-            // } else if (e.key === "Tab") {
-                /**
-                 *  TODO TAB HANDLER
-                 */
-        });
     }
 
     componentWillUnmount() {
