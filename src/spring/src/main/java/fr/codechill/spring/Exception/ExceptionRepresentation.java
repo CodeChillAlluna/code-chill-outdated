3e72df409eaa44c80fe6806fb5f3f71c600fdada
MichanolNathan <nmichanol92@gmail.com>
2018-03-30 12:31:52 +0200

    add: custom handler exception

---
+++ src/spring/src/main/java/fr/codechill/spring/Exception/ExceptionRepresentation.java
@@ -0,0 +1,9 @@
+package fr.codechill.spring.Exception;
+
+public class ExceptionRepresentation {
+    String message;
+
+    public ExceptionRepresentation (String unMessage){
+        this.message = unMessage;
+    }
+}
